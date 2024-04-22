e8d3e2f603be8630fc86c8b4293dd793b83b3aa1
Roilodev <156539959+Roilodev@users.noreply.github.com>
2024-04-22 16:36:01 -0400

    Update Index.blade.php
    First test to create branch

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>RH - Estadísticas</title>
+    <title>RH - Estadísticas 2</title>
 
     <!-- Custom fonts for this template-->
     <link href="{{ asset('assets/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
